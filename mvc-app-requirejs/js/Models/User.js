0e5df4329cf1f978492536e940d82712e3a0ea35
David Gowrie <david.gowrie@gmail.com>
2014-07-15 21:17:06 -0400

    Implementing the Model

--- mvc-app-requirejs/js/Models/User.js
+++ mvc-app-requirejs/js/Models/User.js
@@ -1,1 +1,12 @@
 // User.js
+
+
+// a User will be a simple class with a name attribute:
+define(function(){
+	 
+	function User(name){
+		this.name = name || 'Default name';
+	}
+	 
+	return User;
+});
--- mvc-app-requirejs/js/main.js
+++ mvc-app-requirejs/js/main.js
@@ -4,3 +4,22 @@ require(['MyMath'], function(MyMath){
 	console.log(MyMath.add(1, 2));  
  
 });
+
+
+// Create set of users - declares dependency to User.js model
+require(['Models/User'], function(User){
+	 
+	// manually create a set of users
+	// @NOTE: this could be done in a for loop interation, passing in data as the User args
+	 
+	var users = [new User('Barney'),
+				 new User('Cartman'),
+				 new User('Sheldon')];
+
+	for (var i = 0, len = users.length; i < len; i++){
+		console.log(users[i].name);
+	}
+	 
+	// serialize in JSON the users array and store it in the HTML5 local storage
+	localStorage.users = JSON.stringify(users);
+});
